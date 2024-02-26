ffaa6f1bcd288e0dfb14378c77cc419ef3ead779
HenrikVP <hpo@tec.dk>
2024-02-26 09:11:44 +0100

    Add project files.

---
+++ SocketH4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SocketH4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            IPEndPoint? iPEndPoint = GetIPEndPoint();
+            if (iPEndPoint != null) { new SocketServer(iPEndPoint); }
+            Console.WriteLine("No Ip EndPoint found!!!");
+        }
+
+        static IPEndPoint? GetIPEndPoint()
+        {
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(Dns.GetHostName(), AddressFamily.InterNetwork);
+            IPAddress[] addressList = ipHostEntry.AddressList;
+            //TODO Pick IP Address more intelligenteliently
+            if (addressList == null || addressList[0] == null) return null;
+            return new IPEndPoint(addressList[0], 8090);
+        }
+    }
+}
